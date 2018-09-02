efd3753345af251a061742c652737d1f1f436334
tarasovc47 <tarasovc47@gmail.com>
2018-09-02 14:08:36 +0500

    Промежуточный итог. Готовы: 1) ЧПУ 2) авторизация 3) доступ к разделам сайта закрыт для гостей (не полностью, только по ссылкам в шапке сайта) 4) полностью готов CRUD-генератор пользователей 5) готов, но требует доработки CRUD-генератор аварий (маска для IP-адреса, выбор исполнителя из списка именно исполнителей, более продуманный выбор даты) не готовы: 1) разграничение в правах между группами пользователей (что включает в себя отдельные виды для каждой группы пользователей) 2) в конкретной аварии нет кнопки, меняющей её статус

---
+++ sql/journal.sql
@@ -0,0 +1,135 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Сен 02 2018 г., 11:46
+-- Версия сервера: 5.7.20
+-- Версия PHP: 7.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `journal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `migration`
+--
+
+CREATE TABLE `migration` (
+  `version` varchar(180) NOT NULL,
+  `apply_time` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `migration`
+--
+
+INSERT INTO `migration` (`version`, `apply_time`) VALUES
+('m000000_000000_base', 1535856255),
+('m180902_024212_create_trouble_table', 1535856256),
+('m180902_024216_create_user_table', 1535856257),
+('m180902_024222_create_comment_table', 1535856257);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `trouble`
+--
+
+CREATE TABLE `trouble` (
+  `id` int(11) NOT NULL,
+  `physical_address` text,
+  `ip_address` text,
+  `executor` text,
+  `author` text,
+  `deadline` date DEFAULT NULL,
+  `comment` text NOT NULL,
+  `stages` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `trouble`
+--
+
+INSERT INTO `trouble` (`id`, `physical_address`, `ip_address`, `executor`, `author`, `deadline`, `comment`, `stages`) VALUES
+(1, 'Ленина', '192.168.1.1', 'Исполнитель  1', 'Администратор', '2018-09-02', 'коммент', 'Создана'),
+(3, 'Адрес 2', '192.168.1.1', 'Исполнитель 2', 'Администратор', '2018-09-02', 'коммент 2', 'Создана');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` text,
+  `password` text,
+  `role` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `password`, `role`) VALUES
+(1, 'Администратор', '1', 'admin'),
+(3, 'Исполнитель', '1', 'executor'),
+(4, 'Наблюдатель', '1', 'observer'),
+(5, 'Оператор', '1', 'operator');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `migration`
+--
+ALTER TABLE `migration`
+  ADD PRIMARY KEY (`version`);
+
+--
+-- Индексы таблицы `trouble`
+--
+ALTER TABLE `trouble`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `trouble`
+--
+ALTER TABLE `trouble`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT для таблицы `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
